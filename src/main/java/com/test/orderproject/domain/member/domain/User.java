9e23ca9803e4b10a3435f1911759d529298d742f
wooyong99 <wulovesk@naver.com>
2024-06-06 03:32:51 +0900

    refactor : 사용자 테이블 이름 변경

--- src/main/java/com/test/orderproject/domain/member/domain/User.java
+++ src/main/java/com/test/orderproject/domain/member/domain/User.java
@@ -2,6 +2,7 @@ package com.test.orderproject.domain.member.domain;
 
 import com.test.orderproject.domain.member.model.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name="users")
 public class User extends BaseEntity {
 
     private String email;
