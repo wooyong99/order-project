9759a53cc2353782e748393f0bdc3191a335c261
wooyong99 <wulovesk@naver.com>
2024-08-16 14:07:53 +0900

    rebase test

--- src/test/java/com/test/orderproject/OrderProjectApplicationTests.java
+++ src/test/java/com/test/orderproject/OrderProjectApplicationTests.java
@@ -13,6 +13,7 @@ class OrderProjectApplicationTests {
         System.out.println("test");
         System.out.println("test");
         System.out.println("test");
+        System.out.println("test");
     }
 
 }
