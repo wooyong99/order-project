e622ce332631371851fa1442247e341df3b2e1c9
wooyong99 <wulovesk@naver.com>
2024-08-16 12:52:02 +0900

    rebase test

--- src/test/java/com/test/orderproject/OrderProjectApplicationTests.java
+++ src/test/java/com/test/orderproject/OrderProjectApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OrderProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
 
 }
